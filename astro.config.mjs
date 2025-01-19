ae7ddcf43acc3b279fa974224cbab18709d1e1af
feryardi70 <fery.ardiansyah94747@gmail.com>
2025-01-19 08:22:24 +0700

    2nd commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), partytown()]
+});
